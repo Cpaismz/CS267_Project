ddedd7dde1cd5aea52b01d8a2e0aa3a431ac8d1a
Cpaismz <cpaismz@gmail.com>
2018-04-24 08:24:51 -0300

    SpottingFBP still with minor errors!
    Check CP comment in the last part of the cpp file

--- Simulator/Serial/other/AlphaVersion/SpottingFBP.cpp
+++ Simulator/Serial/other/AlphaVersion/SpottingFBP.cpp
@@ -1,16 +1,25 @@
+// Class inclusions
 #include "SpottingFBP.h"
+#include "CellsFBP.h" 
+
+// Include libraries
 #include <stdio.h>
+#include <string>
+#include <vector>
 #include <math.h>
+#include <cmath>
+#include <iostream>
 #include <unordered_map>
 #include <unordered_set>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::vector<std::vector<int>> coordCells, std::unordered_set<int> AvailSet, double WSD, double WSC, std::unordered_map<std::string, double> spottingParams, bool verbose
-) {
-    // TODO data structures should be passed by reference   
+std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::vector<std::vector<int>> coordCells, 
+											std::unordered_set<int> AvailSet, double WSD, double WSC, 
+											std::unordered_map<std::string, double> spottingParams, bool verbose) {
+    
+	// TODO data structures should be passed by reference   
     // TODO
     std::unordered_map<int, std::unordered_map<int, int>> Angles = std::unordered_map<int, std::unordered_map<int, int>>();
     std::unordered_map<int, std::unordered_map<int, int>> Distances = std::unordered_map<int, std::unordered_map<int, int>>();
@@ -21,11 +30,10 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
     }
 
     double WTolerance = spottingParams["SPOTANGLE"] / 2.0;
-    std::cout << "debug AvailSet size=" << AvailSet.size(); << std::endl;
+    std::cout << "debug AvailSet size=" << AvailSet.size() << std::endl;
 
     double WA = WSD - WTolerance;
     double WB = WSD + WTolerance;
-
 
     double WWindowA = WA;
     double WWindowB = WB;
@@ -43,9 +51,11 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
     }
 
     // TODO: maybe true debugging
-    std::cout << "debug obj keys size " << Cells_Obj.size();
+    /*
+	std::cout << "debug obj keys size " << Cells_Obj.size();
     std::cout << "debug AvailSet size " << AvailSet.size();
     std::cout << "debug CoordCells size " << CoordCells.size();
+	*/
 
     for (auto & _c1 : Cells_Obj) {
         int c1 = _c1.first;
@@ -54,9 +64,9 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
         for (auto & c2 : AvailSet){
          
             if (c1 == c2) {
-                // TODO: can't be None
-                Angles[c1][c2] = None;
-                Distances[c1][c2] = None;
+                // CP: None = -1
+                Angles[c1][c2] = -1;
+                Distances[c1][c2] = -1;
             } else {
                 int a = coordCells[c1][0] - coordCells[c2 - 1][0];
                 int b = coordCells[c1][1] - coordCells[c2 - 1][1];
@@ -64,30 +74,34 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
                 if (a == 0) {
                     if (b >= 0) 
                         Angles[c1][c2] = 270;
-                    else
+                    else{
                         Angles[c1][c2] = 90;
-                    Distances[c1][c2] = cmath::abs(b);
+                    Distances[c1][c2] = std::abs(b);
+					}
                 }
                 if (b == 0) {
                     if (a >= 0) 
                         Angles[c1][c2] = 180;
-                    else
+                    else{
                         Angles[c1][c2] = 0;
-                    Distances[c1][c2] = cmath::abs(a);
+                    Distances[c1][c2] = std::abs(a);
+					}
                 }
-            }
-            if (a != 0 && b != 0) {
-                double radToDeg = 180 / cmath::M_PI;
-                double angle = cmath::atan(b * 1.0 / a) * radToDeg;
-                double distance = cmath::sqrt(a * a + b * b);
+            
+				if (a != 0 && b != 0) {
+					double radToDeg = 180 / M_PI;
+					double angle = std::atan(b * 1.0 / a) * radToDeg;
+					double distance = std::sqrt(a * a + b * b);
 
-                if (a > 0)
-                    angle += 180;
-                if (a < 0 && b > 0)
-                    angle += 360;
-                Angles[c1][c2] = angle;
-                Distances[c1][c2] = distance;
-            }
+					if (a > 0)
+						angle += 180;
+					if (a < 0 && b > 0){
+						angle += 360;
+						Angles[c1][c2] = angle;
+						Distances[c1][c2] = distance;
+					}
+				}
+			}
         }
     }
     if (verbose) {
@@ -96,9 +110,9 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
     }
 
     int anyCell = Cells_Obj.begin()->first;
-    double cellsize = Cells_Obj[anyCell].Perimeter / 4.0;
-    double beta = SpottingParams["SPOT0PROB"];
-    double tilde_d = SpottingParams["SPOT10TIME"] * WSC / cellsize;
+    double cellsize = Cells_Obj[anyCell].perimeter / 4.0;
+    double beta = spottingParams["SPOT0PROB"];
+    double tilde_d = spottingParams["SPOT10TIME"] * WSC / cellsize;
     double alpha = std::log(.1) / tilde_d + std::log(beta) / tilde_d;
 
     for (auto & _c1 : Cells_Obj) {
@@ -106,7 +120,7 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
         SpotProb[c1] = std::unordered_map<int, double>();
         for (auto & c2 : AvailSet) {
             // TODO: can't be None
-            if (angles[c1][c2]  == None) {
+            if (Angles[c1][c2]  == -1) {
                 SpotProb[c1][c2] = 0;
             } else {
                 SpotProb[c1][c2] = beta * std::exp(-1 * Distances[c1][c2] * alpha);
@@ -124,7 +138,7 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
                 WB_t = WB;
                 
                 if (WA_t < 0 && WB_t >= 0 && WB_t < 360 && WTolerance < 180) {
-                    if (!(Angles[c1][c2] <= WB_t || Angles[c1][c2] >= WA_t + 360) {
+                    if (!(Angles[c1][c2] <= WB_t || Angles[c1][c2] >= WA_t + 360)) {
                         SpotProb[c1][c2] = 0;
                     }
                 }
@@ -136,10 +150,24 @@ std::vector<int> SpottingFBP(std::unordered_map<int, CellsFBP> Cells_Obj, std::v
         std::cout << "Probabilities size: " << SpotProb.size() << std::endl;
     }
 
-    std::vector<int> ret = vector<int>();
+	
+	// TODO: CHECK THIS FINAL STEP!!!!!! NOT COMPILING DUE TO C2 out of scope
+	// MODIFYING!!!!  got rid of  msg_list out of scope, got rid of c2 problem, BUT CHECK!!!!!!!!!!
+	// COMPILER THROWS ERROR: 
+	
+	/*
+	In file included from SpottingFBP.h:4:0,
+                 from SpottingFBP.cpp:2:
+CellsFBP.h:57:9: note: candidate: CellsFBP::CellsFBP(int, double, std::vector<int>, double, int, std::__cxx11::string, double, double, int, std::unordered_map<std::__cxx11::basic_string<char>, std::vector<int> >&, std::vector<double>, int, bool)
+         CellsFBP(int _id, double _area, std::vector<int> _coord, double _age,
+	
+	*/
+    std::vector<int> msg_list = vector<int>();
+	
     for (auto & _c1 : SpotProb) {
         int c1 = _c1.first;
         for (auto & _c2 : SpotProb[c1]) {
+			int c2 = _c2.first;
             double random = 1.0 * std::rand() / RAND_MAX; 
             if (SpotProb[c1][c2] > 0 && SpotProb[c1][c2] < random) {
                 if (verbose) {
